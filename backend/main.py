57cecefb599bf3fce97e424e0db1067e715d18d0
Aqil Ahmed <go34hoh@tum.de>
2025-12-21 02:38:31 +0100

    add endpoints

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/api/greeting")
 def read_greeting():
     return {"message": "Hello from the backend!"}
+
+@app.get("/api/add")
+def add(a: int, b: int):
+    return {"a": a, "b": b, "sum": a + b}
